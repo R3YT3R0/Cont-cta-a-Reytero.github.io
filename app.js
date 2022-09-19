0564c6bfec5b95831bb21354630e2a55a9d14b80
reynier <xreyniervx@gmail.com>
2022-09-18 23:24:15 -0400

    Pagina Contácto Reytero

---
+++ app.js
@@ -0,0 +1,5 @@
+function enviar(){
+          var nombreDeUsuario=document.getElementById("NombreUsuario").value;
+          var correoDeUsuario=document.getElementById("CorreoUsuario").value;
+          alert("hola"+" "+nombreDeUsuario + "." + "Vas a Enviar un mensaje desde " + correoDeUsuario )
+}
